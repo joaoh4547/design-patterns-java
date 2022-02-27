d69eb2aa85dbe614e0a33006c103363bb3fb8655
João Henrique <joaohenriquetdj@gmail.com>
2022-02-27 13:18:43 -0300

    create adapter project

---
+++ adapter/src/com/github/joaoh4547/adapter/Application.java
@@ -0,0 +1,7 @@
+package com.github.joaoh4547.adapter;
+
+public class Application {
+	public static void main(String[] args) {
+
+	}
+}
--- builder/src/com/github/joaoh4547/builder/Application.java
+++ builder/src/com/github/joaoh4547/builder/Application.java
@@ -1,8 +1,28 @@
 package com.github.joaoh4547.builder;
 
+import com.github.joaoh4547.builder.builders.CarBuilder;
+import com.github.joaoh4547.builder.builders.TruckBuilder;
+import com.github.joaoh4547.builder.cars.Car;
+import com.github.joaoh4547.builder.cars.Truck;
+import com.github.joaoh4547.builder.director.Director;
+
 public class Application {
 	
 	public static void main(String[] args) {
+		Director director = new Director();
 		
+		//criando carro 
+		
+		CarBuilder builder = new CarBuilder();
+		director.constructSedanCar(builder);
+		Car car = builder.build();
+		
+		System.out.println(String.format("%s produzido com sucesso\n\n", car.getCarType()));
+		
+		TruckBuilder truckBuilder = new TruckBuilder();
+		director.constructTruck(truckBuilder);
+		Truck truck = truckBuilder.build();
+		
+		System.out.println(String.format("Caminhão %s ", truck.result()));
 	}
 }
